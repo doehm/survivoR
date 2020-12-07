d0ab0a06447b9289a6d00168348a3e26362d7855
Daniel Oehm <danieloehm@gmail.com>
2020-12-07 22:11:25 +1100

    BUG fixing vote history, season summary and jury vote bugs. All tests pass

--- tests/testthat/test-vote-history.R
+++ tests/testthat/test-vote-history.R
@@ -4,32 +4,10 @@ library(stringr)
 
 context("vote history")
 
-
 test_that("there should be no missing orders", {
 
   no_missing_values <- all(!is.na(vote_history$order))
   expect_equal(no_missing_values, TRUE)
-
-})
-
-test_that("the 'nickname' list on 'castaways' and 'castaway' on 'vote_history' should be equivalent", {
-
-  nick <- castaways %>%
-    distinct(season, nickname) %>%
-    split(.$season) %>%
-    map(~.x$nickname)
-
-  cast <- vote_history %>%
-    distinct(season, castaway) %>%
-    split(.$season) %>%
-    map(~.x$castaway)
-
-  check <- map2_lgl(nick, cast, ~all(.x %in% .y)) %>%
-    unname() %>%
-    which()
-
-  # expect_equal(check, 1:40)
-  expect_equal(TRUE, TRUE)
 
 })
 
@@ -42,5 +20,54 @@ test_that("there should be no season names in state", {
   check <- all(check[!is.na(check)])
 
   expect_equal(check, TRUE)
+
+})
+
+test_that("order for immunity goes from 1:n()", {
+
+  check <- immunity %>%
+    group_by(season) %>%
+    summarise(
+      n = n(),
+      max_order = max(order)
+    ) %>%
+    mutate(check = n == max_order) %>%
+    .$check
+
+  expect_equal(all(check), TRUE)
+
+})
+
+test_that("castaways order goes from 1:n()", {
+
+  check <- castaways %>%
+    group_by(season) %>%
+    summarise(
+      n = n(),
+      max_order = max(order)
+    ) %>%
+    mutate(check = n == max_order) %>%
+    .$check
+
+  expect_equal(all(check), TRUE)
+
+})
+
+test_that("jury votes match the outcome", {
+
+  match <- jury_votes %>%
+    group_by(season, finalist) %>%
+    summarise(n = sum(vote)) %>%
+    group_by(season) %>%
+    filter(n == max(n)) %>%
+    left_join(
+      season_summary %>%
+        select(season, winner),
+      by = "season"
+    ) %>%
+    mutate(match = finalist == winner) %>%
+    .$match
+
+  expect_equal(all(match), TRUE)
 
 })
