880736efe7db369b797355b3e642e1cee5ebd3c8
Daniel Oehm <danieloehm@gmail.com>
2021-01-16 18:50:46 +1100

    ADD adding clean_votes which removes the none person votes

---
+++ R/helpers.R
@@ -0,0 +1,31 @@
+# helpers
+
+#' Cleans votes
+#'
+#' There are certain events in the game of survivor which mean someone may attend tribal council and
+#' not get the change to vote for some reason or their vote is unique e.g. when rocks are drawn. You
+#' may want to remove the votes that were not an actual vote for a person. \code{clean_votes} is a
+#' convenience function to remove these records. Can be piped.
+#'
+#' @param df Data frame which must contain the \code{vote} data.
+#'
+#' @importFrom stringr str_detect
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' vh <- vote_history %>%
+#' filter(
+#'   season == 40,
+#'   episode == 10
+#' ) %>%
+#' count(vote)
+#' vh
+#'
+#' vh %>%
+#' clean_votes()
+clean_votes <- function(df) {
+  df %>%
+    filter(!str_detect(tolower(vote), "win|won|lose|immune|none|vote|black rock|white rock|purple rock|yellow rock|exiled|saved|kidnap|countback"))
+}
