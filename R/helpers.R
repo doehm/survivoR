74077746a4cb031ae38e692e92e351cf214c9eef
doehm <danieloehm@gmail.com>
2022-02-12 17:13:42 +1100

    BUG removed native pipe from clean_votes()

--- R/helpers.R
+++ R/helpers.R
@@ -15,7 +15,7 @@
 #'
 #' @examples
 #' library(dplyr)
-#' vh <- vote_history %>%
+#' vh <- vote_history |>
 #' filter(
 #'   season == 40,
 #'   episode == 10
@@ -26,8 +26,7 @@
 #' vh |>
 #' clean_votes()
 clean_votes <- function(df) {
-  df |>
-    filter_at(vars("vote"), ~!str_detect(tolower(.x), "win|won|lose|immune|none|vote|black rock|white rock|purple rock|yellow rock|exiled|saved|kidnap|countback"))
+  filter_at(df, vars("vote"), ~!str_detect(tolower(.x), "win|won|lose|immune|none|vote|black rock|white rock|purple rock|yellow rock|exiled|saved|kidnap|countback"))
 }
 
 
