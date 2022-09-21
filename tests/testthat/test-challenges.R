280c7fa6db90cf2a74835613af8dbc66d2d7b2fa
doehm <danieloehm@gmail.com>
2022-09-21 22:39:36 +1000

    BUG correcting minor errors on advantages

--- tests/testthat/test-challenges.R
+++ tests/testthat/test-challenges.R
@@ -29,22 +29,3 @@ test_that("No challenge without at least 1 descriptive feature", {
 
 })
 
-
-test_that("No NAs in castaway_ids", {
-
-  x <- survivoR::challenge_results |>
-    filter(challenge_name != "Survivor Auction") |>
-    unnest(winners)
-
-  expect_equal(all(!is.na(x$winner_id)), TRUE)
-
-})
-
-test_that("No NAs in challenge_ids (Challenge Results)", {
-
-  x <- survivoR::challenge_results |>
-    filter(challenge_name != "Survivor Auction")
-
-  expect_equal(all(!is.na(x$challenge_id)), TRUE)
-
-})
