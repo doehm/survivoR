e0948a517b1688ab21ffbcc6e8d5fdbac9a27f50
Daniel Oehm <danieloehm@gmail.com>
2024-02-17 17:41:16 +1100

    DEV reviewing vote history immunity discrepencies

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ winners, jury votes, advantage details and a lot more.
 
 # Installation
 
-Now on CRAN (v2.3.1) or Git (v2.3.1).
+Now on CRAN (v2.3.1) or Git (v2.3.2).
 
 If Git \> CRAN I’d suggest install from Git. We are constantly improving
 the data sets so the github version is likely to be slightly improved.
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -24,3 +24,33 @@ test_that("Correct split votes", {
 
   expect_equal(x, 0)
 })
+
+test_that("No votes for people who have immunity", {
+  immune <- vote_history |>
+    filter(
+      !is.na(immunity),
+      immunity != "Hidden"
+    ) |>
+    distinct(version_season, order, episode, immune_castaway = castaway)
+
+  vote_history |>
+    left_join(
+      immune,
+      by = c("version_season", "episode", "order"),
+      relationship = "many-to-many"
+    ) |>
+    filter(vote == immune_castaway)
+
+  expect_equal(0, 0)
+})
+
+test_that("Individual immunity assigned on vote history", {
+  nobody_immune = survivoR::vote_history |>
+    filter(tribe_status == "Merged") |>
+    filter(vote_order == 1) |>
+    group_by(version_season, version, season, episode, order) |>
+    summarise(immunity_winner = sum(immunity == "Individual", na.rm = TRUE)) |>
+    filter(immunity_winner == 0)
+
+  expect_equal(nrow(nobody_immune), 5)
+})
