a845d98c59a87fd6f65de3822719e74c4cf7e8af
Daniel Oehm <danieloehm@gmail.com>
2024-06-06 23:02:58 +1000

    DEV adding new tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,23 +1,18 @@
 library(dplyr)
 library(stringr)
 
-test_that("no one voted for themselves", {
+
+# VOTE HISTORY ------------------------------------------------------------
+
+test_that("📜 No one voted for themselves", {
   x <- vote_history |>
     filter(castaway == vote)
 
   expect_equal(nrow(x), 0)
 })
 
-test_that("Castaway details is unique", {
-  nrows <- nrow(castaway_details)
-  distinct_rows <- castaway_details |>
-    distinct(castaway_id) |>
-    nrow()
 
-  expect_equal(nrows, distinct_rows)
-})
-
-test_that("Correct split votes", {
+test_that("📜 Correct split votes", {
   x <- vote_history |>
     filter(!is.na(split_vote), !str_detect(split_vote, vote)) |>
     nrow()
@@ -25,7 +20,7 @@ test_that("Correct split votes", {
   expect_equal(x, 0)
 })
 
-test_that("No votes for people who have immunity", {
+test_that("📜 No votes for people who have immunity", {
   immune <- vote_history |>
     filter(
       !is.na(immunity),
@@ -44,7 +39,7 @@ test_that("No votes for people who have immunity", {
   expect_equal(0, 0)
 })
 
-test_that("Individual immunity assigned on vote history", {
+test_that("📜 Individual immunity assigned on vote history", {
   nobody_immune <- vote_history |>
     filter(tribe_status == "Merged") |>
     filter(vote_order == 1) |>
@@ -55,7 +50,7 @@ test_that("Individual immunity assigned on vote history", {
   expect_equal(nrow(nobody_immune), 4)
 })
 
-test_that("Winners on challenge_results match immunity on vote_history", {
+test_that("📜 Winners on challenge_results match immunity on vote_history", {
   immunity_winners <- challenge_results |>
     filter(
       outcome_type == "Individual",
@@ -81,7 +76,197 @@ test_that("Winners on challenge_results match immunity on vote_history", {
   expect_equal(x1, 14)
 })
 
-test_that("Jury votes matches 'jury' on castaways", {
+test_that("📜 Vote event consistency", {
+
+  x1 <- vote_history |>
+    count(vote_event) |>
+    nrow()
+
+  x2 <- vote_history |>
+    mutate(vote_event = tolower(vote_event)) |>
+    count(vote_event) |>
+    nrow()
+
+  expect_equal(x1, x2)
+
+})
+
+
+test_that("📜 Vote event outcome consistency", {
+
+  x1 <- vote_history |>
+    count(vote_event_outcome) |>
+    nrow()
+
+  x2 <- vote_history |>
+    mutate(vote_event_outcome = tolower(vote_event_outcome)) |>
+    count(vote_event_outcome) |>
+    nrow()
+
+  expect_equal(x1, x2)
+
+})
+
+test_that("📜 No new things in vote event", {
+
+  acceptable_values <- c('Deadlock', 'Final 3 tribal', 'Countback', 'Nature quiz', 'Rock draw', 'Kidnapped', 'Quit', 'Fire challenge', 'Exiled', 'Won immunity challenge', 'Extra vote', 'Steal a vote', 'Unanimous decision', 'Vote blocker', 'Abstain to gain', 'Fire challenge (f4)', 'Ghost island game', 'Island of the idols game', 'Safety without power', 'Beware advantage', 'Shot in the dark', 'Do or die', 'Summit', 'Bank your vote', 'Control the vote', 'Player quit', 'Journey challenge', 'Sacrificed vote to extend idol', 'Sacrificed vote to extend idol; goodwill advantage', 'Lost vote at survivor auction', 'First out in challenge', 'Lost vote on journey', 'Dead man walking', 'Vote to kidnap', 'Trial by fire', 'Sick day', 'Exempt', 'Removed from tribal', 'Lost tribal council reward challenge', 'Ultimate vote played successfully', 'Black cowrie', 'Tiebreaker challenge', 'Island of secrets game', 'Traded vote', 'Stayed on immunity island', 'Tied destiny', 'Tribal council pass', 'No vote', 'Sudden death trivia', 'Vote stolen')
+
+  vote_history |>
+    filter(
+      !vote_event %in% acceptable_values,
+      !is.na(vote_event)
+    ) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("📜 No new things in vote event outcome", {
+
+  acceptable_values <- c('Can\'t vote', 'Vote not required', 'Eliminated', 'Safe', 'Lost', 'Won', 'Immune', 'Removed from tribal', 'No vote', 'Extra vote', 'Lost vote', 'Saved', 'Not safe', 'Forced vote', 'Lost vote; gained vote', 'Exempt', 'Nullified all other votes', 'Additional vote')
+
+  vote_history |>
+    filter(
+      !vote_event_outcome %in% acceptable_values,
+      !is.na(vote_event_outcome)
+    ) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("📜 Castaway IDs OK", {
+
+  vote_history |>
+    distinct(version_season, castaway, castaway_id) |>
+    group_by(version_season, castaway) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("📜 Vote IDs OK", {
+
+  vote_history |>
+    distinct(version_season, vote, vote_id) |>
+    group_by(version_season, vote) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("📜 Voted out IDs OK", {
+
+  vote_history |>
+    distinct(version_season, voted_out, voted_out_id) |>
+    group_by(version_season, voted_out) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+# CHALLENGES --------------------------------------------------------------
+
+test_that("🏆 Challenge summary and challenge results are the same size", {
+
+  x1 <- challenge_summary |>
+    distinct(version_season, challenge_id) |>
+    nrow()
+
+  x2 <- challenge_results |>
+    filter(version == "US") |>
+    distinct(version_season, challenge_id) |>
+    nrow()
+
+  expect_equal(x1, x2)
+
+})
+
+
+test_that("🏆 No castaway duplication within a challenge", {
+
+  challenge_results |>
+    group_by(version_season, challenge_id, castaway) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("🏆 Challenge type consistency", {
+
+  acceptable_types <- c('Immunity and Reward', 'Reward', 'Immunity', 'Duel', 'Captains Duel')
+
+  challenge_results |>
+    filter(!challenge_type %in% acceptable_types) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+test_that("🏆 No incorrect castaway IDs", {
+
+  challenge_results |>
+    distinct(version_season, castaway, castaway_id) |>
+    group_by(version_season, castaway) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+
+
+# CASTAWAYS ---------------------------------------------------------------
+
+test_that("🧑 Castaway details is unique", {
+  nrows <- nrow(castaway_details)
+  distinct_rows <- castaway_details |>
+    distinct(castaway_id) |>
+    nrow()
+
+  expect_equal(nrows, distinct_rows)
+})
+
+test_that("🧑 No more than one winner", {
+  x <- castaways |>
+    group_by(version_season) |>
+    summarise(
+      n_jury = sum(jury),
+      n_finalist = sum(finalist),
+      n_winner = sum(winner)
+    ) |>
+    filter(n_winner != 1) |>
+    nrow()
+
+  expect_equal(x, 0)
+})
+
+
+test_that("🧑 Consistent results", {
+
+  acceptable_values <- c('10th voted out', '11th voted out', '12th voted out', '13th voted out', '14th voted out', '15th voted out', '16th voted out', '17th voted out', '18th voted out', '19th voted out', '1st voted out', '20th voted out', '21st voted out', '22nd voted out', '23rd voted out', '2nd runner-up', '2nd voted out', '2nd voted out; Quit EoE', '3rd voted out', '3rd voted out; Quit', '4th voted out', '5th voted out', '6th voted out', '6th voted out; Quit EoE', '7th voted out', '8th voted out', '8th voted out; Quit EoE', '9th voted out', 'Ejected', 'Eliminated', 'Evacuated', 'Lost final 4 fire challenge', 'Lost fire challenge', 'Medically evacuated', 'Quit', 'Runner-up', 'Sole Survivor', 'Switched', 'Tied destiny', 'Withdrew')
+
+  castaways |>
+    filter(!result %in% acceptable_values) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+# JURY --------------------------------------------------------------------
+
+test_that("👩‍⚖️ Jury votes matches 'jury' on castaways", {
 
   jury <- survivoR::jury_votes |>
     group_by(version_season) |>
@@ -102,36 +287,8 @@ test_that("Jury votes matches 'jury' on castaways", {
 
 })
 
-test_that("Challenge summary and challenge results are the same size", {
-  x1 <- challenge_summary |>
-    distinct(version_season, challenge_id) |>
-    nrow()
 
-  x2 <- challenge_results |>
-    filter(version == "US") |>
-    distinct(version_season, challenge_id) |>
-    nrow()
-
-  expect_equal(x1, x2)
-})
-
-
-test_that("More than one winner", {
-  x <- castaways |>
-    group_by(version_season) |>
-    summarise(
-      n_jury = sum(jury),
-      n_finalist = sum(finalist),
-      n_winner = sum(winner)
-    ) |>
-    filter(n_winner != 1) |>
-    nrow()
-
-  expect_equal(x, 0)
-})
-
-
-test_that("Jury count the same on castaways and jury votes", {
+test_that("👩‍⚖️ Jury count the same on castaways and jury votes", {
 
   x <- castaways |>
     group_by(version_season) |>
@@ -159,7 +316,47 @@ test_that("Jury count the same on castaways and jury votes", {
 })
 
 
-test_that("Advatnage Type consistency", {
+test_that("👩‍⚖️ Finalist IDs OK", {
+
+  jury_votes |>
+    distinct(version_season, finalist, finalist_id) |>
+    group_by(version_season, finalist) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("👩‍⚖️ Castaway IDs OK", {
+
+  jury_votes |>
+    distinct(version_season, castaway, castaway_id) |>
+    group_by(version_season, castaway) |>
+    filter(n() > 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("👩‍⚖️ The number of votes equals the number of jurors", {
+
+  jury_votes |>
+    group_by(version_season) |>
+    summarise(
+      n_jurors = n_distinct(castaway),
+      n_votes = sum(vote)
+    ) |>
+    filter(n_jurors != n_votes) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+# ADVANTAGES --------------------------------------------------------------
+
+test_that("📿 Advantage Type consistency", {
 
   x1 <- advantage_details |>
     count(advantage_type) |>
@@ -175,39 +372,9 @@ test_that("Advatnage Type consistency", {
 })
 
 
-test_that("Vote event consistency", {
-
-  x1 <- vote_history |>
-    count(vote_event) |>
-    nrow()
-
-  x2 <- vote_history |>
-    mutate(vote_event = tolower(vote_event)) |>
-    count(vote_event) |>
-    nrow()
-
-  expect_equal(x1, x2)
-
-})
 
 
-test_that("Vote event outcome consistency", {
-
-  x1 <- vote_history |>
-    count(vote_event_outcome) |>
-    nrow()
-
-  x2 <- vote_history |>
-    mutate(vote_event_outcome = tolower(vote_event_outcome)) |>
-    count(vote_event_outcome) |>
-    nrow()
-
-  expect_equal(x1, x2)
-
-})
-
-
-test_that("No advantage ID's are missing", {
+test_that("📿 No advantage ID's are missing", {
 
   advantage_details |>
     filter(is.na(advantage_id)) |>
@@ -217,7 +384,7 @@ test_that("No advantage ID's are missing", {
 })
 
 
-test_that("Advantage sequence ID is in sequence", {
+test_that("📿 Advantage sequence ID is in sequence", {
 
   advantage_movement |>
     group_by(version_season, advantage_id) |>
@@ -234,7 +401,7 @@ test_that("Advantage sequence ID is in sequence", {
 })
 
 
-test_that("There are no advantage ID dupes", {
+test_that("📿 There are no advantage ID dupes", {
 
   advantage_details %>%
     filter(!is.na(advantage_id)) %>%
@@ -248,7 +415,7 @@ test_that("There are no advantage ID dupes", {
 })
 
 
-test_that("Advantage movement and details are synced", {
+test_that("📿 Advantage movement and details are synced", {
 
   advantage_movement %>%
     anti_join(advantage_details, join_by(version, season, advantage_id)) %>%
@@ -260,7 +427,7 @@ test_that("Advantage movement and details are synced", {
 })
 
 
-test_that("There are sequential advantage IDs", {
+test_that("📿 There are sequential advantage IDs", {
 
   advantage_details %>%
     select(version, season, advantage_id) %>%
@@ -271,5 +438,90 @@ test_that("There are sequential advantage IDs", {
     filter(max_advantage_id != n) |>
     nrow() |>
     expect_equal(1)
+
+})
+
+
+test_that("📿 Consistent advantage categories", {
+
+  acceptable_types <- c('Hidden Immunity Idol', 'Super Idol', 'Extra Vote', 'Steal a Vote', 'Reward Stealer', 'Vote Blocker', 'Hidden Immunity Idol Half', 'Idol Nullifier', 'Advantage Menu', 'Knowledge is Power', 'Amulet', 'Choose your Champion', 'Challenge Advantage', 'Bank your Vote', 'Inheritance Advantage', 'Control the Vote', 'Safety without Power', 'Goodwill Advantage', 'Kidnap Castaway from Other Tribe', 'Moral Dilemma', 'Remove Jury Member', 'Vote Steal', 'Voter Remover', 'Ultimate Vote', 'Disadvantage Future Vote Cast Against you', 'Black Cowrie', 'Hidden Immunity Idol Clue', 'White Cowrie', 'Practice Advantage', 'Diplomatic Immunity', 'Tribal Council Pass', 'Outsurance Reward Send Token', 'Save the Date', 'Coin Flip')
+
+  advantage_details |>
+    filter(!advantage_type %in% acceptable_types) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+# BOOT MAPPING ------------------------------------------------------------
+
+test_that("🥾 No dupes in boot mapping", {
+
+  expect_equal(
+    boot_mapping |>
+      nrow(),
+    boot_mapping |>
+      distinct() |>
+      nrow()
+  )
+
+})
+
+test_that("🥾 Castaway IDs are OK", {
+
+  boot_mapping |>
+    group_by(version_season, episode, order, castaway) |>
+    filter(n()>1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("🥾 Final N is OK", {
+
+  boot_mapping |>
+    filter(!is.na(final_n)) |>
+    group_by(version_season) |>
+    summarise(
+      n = n_distinct(castaway[order == 0]),
+      min = min(final_n),
+      max = max(final_n)
+    ) |>
+    filter(n != max | min != 1) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+test_that("🥾 Final N matches the number of castaways and IDs", {
+
+  boot_mapping |>
+    filter(!is.na(final_n)) |>
+    group_by(version_season, final_n) |>
+    summarise(
+      n_castaway = n_distinct(castaway),
+      n_castaway_id = n_distinct(castaway_id)
+    ) |>
+    filter(final_n != n_castaway | final_n != n_castaway_id) |>
+    nrow() |>
+    expect_equal(0)
+
+})
+
+
+# TRIBE MAPPING -----------------------------------------------------------
+
+test_that("🧜‍♂️ No dupes in tribe mapping", {
+
+  expect_equal(
+    tribe_mapping |>
+      nrow(),
+    tribe_mapping |>
+      distinct() |>
+      nrow()
+  )
 
 })
